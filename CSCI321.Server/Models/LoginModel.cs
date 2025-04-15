77d8a6842cde72b56facb7427afebb738abd4745
jlhitchcock <113740147+jlhitchcock@users.noreply.github.com>
2025-04-15 20:59:16 +1000

    deleteing backend

--- CSCI321.Server/Models/LoginModel.cs
+++
@@ -1,9 +0,0 @@
-
-namespace CSCI321.Server.Models {
-public class LoginModel
-    {
-        public string UserType { get; set; }  // User ID (if applicable)
-        public string Email { get; set; }    // User email
-        public string Password { get; set; }  // User password
-    }
-}
--- CSCI321.Server/Models/PasswordForm.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CSCI321.Server.Models;
-
-public class PasswordForm
-{
-    public string oldPassword {get; set;}
-    public string newPassword {get; set;}
-    public string userId { get; set; }
-}
